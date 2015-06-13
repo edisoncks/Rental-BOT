25d4ce62ab49f7f40eab62b0fbe01e7fb5ac7419
edisoncks <kurai.no.yozora@gmail.com>
2015-06-14 05:02:02 +0800

    Edited share qr code activity

--- app/src/main/java/com/android/rental_bot/ShareQR.java
+++ app/src/main/java/com/android/rental_bot/ShareQR.java
@@ -1,9 +1,15 @@
 package com.android.rental_bot;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 
 public class ShareQR extends ActionBarActivity {
@@ -12,6 +18,32 @@ public class ShareQR extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_qr);
+
+        final WebView webView= (WebView)findViewById(R.id.webviewShareQRcode);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new CustomWebViewClient());
+
+        webView.loadUrl("http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=QWERTY&chld=H");
+
+        Button share = (Button)findViewById(R.id.btnShareQRShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=QWERTY&chld=H");
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent,getResources().getText(R.string.send_to)));
+            }
+        });
+    }
+
+    private class CustomWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
     }
 
     @Override
